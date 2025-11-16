eee82ac667f34ec0c8e74c1c7938a77d4aee0811
blankkywired <blankky@protonmail.com>
2025-11-15 22:23:23 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+
+data = [
+    {
+        "name": 'Instagram',
+        'follower_count': 346,
+        'description': 'A social media plataform',
+        'country': 'Unite States Of America'
+    },
+
+    {
+        "name": 'Kwai',
+        'follower_count': 54,
+        'description': 'A social media plataform',
+        'country': 'South Korea'
+    },
+
+    {
+        "name": 'Tik tok',
+        'follower_count': 566,
+        'description': 'A social media plataform',
+        'country': 'England'
+    }
+
+]
+
+#print(data[0]["name"])
+
+for i in data[0]:
+    print(data[0][i])
+
+print('\n')
+
+for i in data[1]:
+    print(data[1][i])
+
+print('\n')
+
+for i in data[2]:
+    print(data[2][i])
+
