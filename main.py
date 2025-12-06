83bb76fe9717abde11127ccc4ad3f1253a9448c3
blankkywired <blankky@protonmail.com>
2025-12-06 20:45:33 -0300

    cleaned lines

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import random
 import dictbase
 import draws
 
-#Escolha inicial
-
-
-#Evita que ocorra escolhas semelhantes entre as duas opções
-
-
-#print(first_Choice)
-#print(second_Choice)
-
-
 print(f"{draws.image_Logo}\n\n")
 
 gameStart = True
@@ -19,11 +9,11 @@ score = 0
 def main():
     global user_question
     print(f"\nCompare A: {first_Choice['name']} a {first_Choice['description']}\n{draws.vs_Logo}\n\nAgainst B: {second_Choice['name']}, a {second_Choice['description']}")
-
     user_question = input("Who has more followers? Type 'A' or 'B': ")
+
     check_answer(user_question)
     
-#Preciso criar uma função aqui para checar as respostas do usuario
+# Check user answer
 def check_answer(answer):
     global score
     if user_question == "A":
@@ -33,13 +23,17 @@ def check_answer(answer):
         else:
             print(f"\nSorry, you're wrong, Final Score: {score}")
             #Parar o loop
-            score = -1
+            gameStart = False
 
     elif user_question == "B":
         if second_Choice['follower_count'] > first_Choice['follower_count']:
             score += 1
             print(f"\nYou're right!, Current score: {score}")
+
+            #Saving new informations about the choice
             first_Choice['follower_count'] = second_Choice['follower_count']
+            first_Choice['name'] = second_Choice['name']
+            first_Choice['description'] = second_Choice['description']
         else:
             print(f"\nSorry, you're wrong, Final Score: {score}")
             score = -1 
@@ -50,18 +44,13 @@ def check_answer(answer):
 
 first_Choice = dict(random.choice(dictbase.data))
 
-#Guardar o resultado do maior palpite
-#highestFollowerCount = dict()
-while gameStart != False:
+
+
+while gameStart:
     if score >= 0:
         second_Choice = dict(random.choice(dictbase.data))
         #Impedir que sejam feita duas escolhas semelhantes
-        while first_Choice == second_Choice:
+        while first_Choice['name'] == second_Choice['name']:
             second_Choice = dict(random.choice(dictbase.data))
         main()
     
-
-
-    
-
-
